737323a7f4d8eb1621b8b85464517d488c46e587
haze <shoou0000@gmail.com>
2018-03-21 17:20:54 +0900

    add 10,11

---
+++ 3_Go_HttpClient/11_FileSystem.go
@@ -0,0 +1,26 @@
+package main
+
+// curl file://haze_icon.png
+
+import (
+	"log"
+	"net/http"
+	"net/http/httputil"
+)
+
+func main() {
+	transport := &http.Transport{}
+	transport.RegisterProtocol("file", http.NewFileTransport(http.Dir(".")))
+	client := http.Client{
+		Transport: transport,
+	}
+	resp, err := client.Get("file://./haze_icon.png")
+	if err != nil {
+		panic(err)
+	}
+	dump, err := httputil.DumpResponse(resp, true)
+	if err != nil {
+		panic(err)
+	}
+	log.Println(string(dump))
+}
