680eb9b8b4d15abc0b419fe4c48f1fafdb48ae5c
haze <shoou0000@gmail.com>
2018-03-24 11:33:27 +0900

    add 12

---
+++ 3_Go_HttpClient/12_DeleteMethod.go
@@ -0,0 +1,29 @@
+package main
+
+// curl -X DELETE http://localhost:18888
+
+import (
+	"log"
+	"net/http"
+	"net/http/httputil"
+)
+
+func main() {
+	client := &http.Client{}
+	request, err := http.NewRequest("DELETE", "http://localhost:18888", nil)
+	if err != nil {
+		panic(err)
+	}
+
+	resp, err := client.Do(request)
+	if err != nil {
+		panic(err)
+	}
+
+	dump, err := httputil.DumpResponse(resp, true) 
+	if err != nil {
+		panic(err)
+	}
+
+	log.Println(string(dump))
+}
